5fa1d77cefee410971da7b0d3f8aa72ae522e0f6
Aryaman11Agarwal <98214060+Aryaman11Agarwal@users.noreply.github.com>
2024-02-16 00:18:14 +0530

    Update SegmentTreeCoinsLazyPropagation.cpp

--- SegmentTreeCoinsLazyPropagation.cpp
+++ SegmentTreeCoinsLazyPropagation.cpp
@@ -1,91 +1,86 @@
-#include <bits/stdc++.h>
-using namespace std;
-//lazy propogation
+#define ll long long
+class SegmentTree{
+    vector<ll> tree,lazy;
+    public:
+    SegmentTree(int n){
+        tree.resize(4*n+1,0);
+        lazy.resize(4*n+1,0);
+    }
 
-class SegTree{
-   vector<int> tree,lazy;
-   public:
-   SegTree(int n){
-       tree.resize(4*n+1,0);
-       lazy.resize(4*n+1,0);
-   }
-   
-   void build(int index,int low,int high,vector<int>&arr){
-       if(low==high){
-           tree[index]=arr[low];
-           return;
-       }
-       
-       int mid=(low+high)/2;
-       
-       build(2*index+1,low,mid,arr);
-       build(2*index+2,mid+1,high,arr);
-       
-       tree[index]=tree[2*index+1]+tree[2*index+2];
-       
-       
-   }
-   
-   void update(int index,int low,int high,int l,int r){
-       if(lazy[index]!=0){
-           tree[index]=!tree[index];
-          if(low!=high){
-              lazy[2*index+1]= !lazy[2*index+1];
-              lazy[2*index+2]=!lazy[2*index+2];
-          }
-          
-          lazy[index]=0;
-          
-       }
-       
-       
-       if(low>r||l>high)
-       return ;
-       
-       else if(l<=low&&high<=r){
-           tree[index]=(high-low+1)-tree[index];
-           if(low!=high){
+    void build(int index,int low,int high,vector<int>&arr){
+        if(low==high)
+        {
+            tree[index]=arr[low];
+            return ;
+        }
+
+
+        int mid=(low+high)/2;
+
+        build(2*index+1,low,mid,arr);
+        build(2*index+2,mid+1,high,arr);
+
+        tree[index]=tree[2*index+1]+tree[2*index+2];
+    }
+
+    void update(int index,int low,int high,int l,int h){
+        if(lazy[index]!=0){
+            tree[index]=(high-low+1)-tree[index];
+            if(high!=low){
                lazy[2*index+1]=!lazy[2*index+1];
                lazy[2*index+2]=!lazy[2*index+2];
-           } 
-           
+            }
+
+            lazy[index]=0;
+            
+        }
+
+        if(l>high||low>h)
+        return ;
+
+        else if(l<=low&&high<=h){
+            tree[index]=(high-low+1)-tree[index];
+             if(high!=low){
+               lazy[2*index+1]=!lazy[2*index+1];
+               lazy[2*index+2]=!lazy[2*index+2];
+            }
+
+            return ;
+        }
+
         
-       }
-       else{
-         int  mid=(low+high)/2;
-           
-           update(2*index+1,low,mid,l,r,val);
-           update(2*index+2,mid+1,high,l,r,val);
-           
-           tree[index]=tree[2*index+1]+tree[2*index+2];
-           
-       }
-   }
-   
-   int query(int index,int low,int high,int l,int h){
-       
-        if(lazy[index]!=0){
-           tree[index]=(high-low+1)-tree[index];
-          if(low!=high){
-              lazy[2*index+1]=!lazy[2*index+1];
-              lazy[2*index+2]=!lazy[2*index+2];
-          }
-          
-          lazy[index]=0;
-          
-       }
-       
-        if(low>h||l>high)
-     return 0;
-     
-    else if(l<=low&&high<=h)
-     return tree[index];
-     
-     
-     else{
-         int mid=(low+high)/2;
-         
-         return (query(2*index+1,low,mid,l,h)+query(2*index+2,mid+1,high,l,h));
-     }
-   }
+       int  mid=(low+high)/2;
+
+         update(2*index+1,low,mid,l,h);
+          update(2*index+2,mid+1,high,l,h);
+
+          tree[index]=tree[2*index+1]+tree[2*index+2];
+
+
+    }
+
+
+    ll query(int index,int low,int high,int l,int h){
+         if(lazy[index]!=0){
+            tree[index]=(high-low+1)-tree[index];
+            if(high!=low){
+               lazy[2*index+1]=!lazy[2*index+1];
+               lazy[2*index+2]=!lazy[2*index+2];
+            }
+
+            lazy[index]=0;
+            
+        }
+
+        if(l>high||low>h)
+        return 0;
+
+        else if(l<=low&&high<=h)
+        return tree[index];
+
+        int mid=(low+high)/2;
+
+       return query(2*index+1,low,mid,l,h)+query(2*index+2,mid+1,high,l,h);
+
+    }
 };
